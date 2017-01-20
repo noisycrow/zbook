7c5ab4dcba61444861c253be4f585f120196e46c
Tom <tutterba@starbucks.com>
2017-01-20 09:05:54 -0800

    Front page HTML now generated from a template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,7 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -20,16 +21,21 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),
+                         'Enter a to-do item')
 
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+        self.assertTrue(any(row.text ==
+                            '1: Buy peacock feathers' for row in rows),
+                        "New to-do item did not appear in the table")
 
         self.fail('Finish the test!')
 
+
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
+    

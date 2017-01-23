fe20652a0b8baed105f4c3e1306ce1dce5a6d652
Tom Utterback <timba@noisycrow.com>
2017-01-23 11:58:42 -0800

    Commit after QA Dev time

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -12,6 +12,7 @@ class HomePageTest(TestCase):
     
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
+
         response = home_page(request)
         expected_html = render_to_string('home.html')
 
@@ -20,4 +21,13 @@ class HomePageTest(TestCase):
         #self.assertIn(b'<title>To-Do lists</title>', response.content)
         #self.assertTrue(response.content.endswith(b'</html>'))
         
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
 
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string('home.html', {'new_item_text': 'A new list item'})
+        self.assertEqual(response.content.decode(), expected_html)

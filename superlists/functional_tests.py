e96229ce3401ccbcd1f5eb553013d0cb4025124b
Tom <tutterba@starbucks.com>
2017-01-24 15:57:58 -0800

    After adding second func test

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -30,6 +30,7 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         self.fail('Finish the test!')
 

cd79c84acd11b99bc6c6af8afe25cb39b5c9aca1
Tom <tutterba@starbucks.com>
2017-02-01 15:05:42 -0800

    Working through some problems

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -13,7 +13,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name("tr")
+        rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
@@ -28,6 +28,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'),
                          'Enter a to-do item')
+
+        import time; time.sleep(3)
 
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)

dc9882a92348c5a7804dab8971ce36da24a16eff
Tom <tutterba@starbucks.com>
2017-02-02 12:25:35 -0800

    Added waits to func tests so they will run

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -13,7 +13,9 @@ class NewVisitorTest(unittest.TestCase):
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
+        import time; time.sleep(3)
         rows = table.find_elements_by_tag_name('tr')
+        import time; time.sleep(3)
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
@@ -34,11 +36,14 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
 
+        import time; time.sleep(3)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
+        import time; time.sleep(3)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 

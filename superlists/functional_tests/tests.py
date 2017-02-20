8955c4f7ad0ce9407e6eb0768e485b6f4be4c71c
Tom Utterback <timba@noisycrow.com>
2017-02-20 11:38:45 -0800

    first steps of FT for layout

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -12,11 +12,19 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2, 512, delta=5)
+        
+
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
-        time.sleep(3)
+        time.sleep(2)
         rows = table.find_elements_by_tag_name('tr')
-        time.sleep(3)
+        time.sleep(2)
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
@@ -32,22 +40,22 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertEqual(inputbox.get_attribute('placeholder'),
                          'Enter a to-do item')
 
-        time.sleep(3)
+        time.sleep(2)
 
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
+        time.sleep(2)
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+')
 
-        time.sleep(3)
+        time.sleep(2)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        time.sleep(3)
+        time.sleep(2)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
